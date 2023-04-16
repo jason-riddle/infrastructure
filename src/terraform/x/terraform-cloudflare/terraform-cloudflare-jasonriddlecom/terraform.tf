1ea415fdfa04ae5cbef3e0b34aaebef205261535
Jason Riddle <hello@jasonriddle.com>
2023-04-16 14:55:50 -0700

    Add cloudflare_records

---
+++ src/terraform/x/terraform-cloudflare/terraform-cloudflare-jasonriddlecom/terraform.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 0.15"
+
+  cloud {
+    organization = "jason-riddle"
+
+    workspaces {
+      name = "terraform-cloudflare-jasonriddlecom"
+    }
+  }
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3"
+    }
+  }
+}
+
+# https://developers.cloudflare.com/fundamentals/api/get-started/create-token/
+# Set CLOUDFLARE_API_TOKEN.
+# provider "cloudflare" {
+#   api_token = var.cloudflare_api_token
+# }
