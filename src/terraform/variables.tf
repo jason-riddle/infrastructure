e31c06cfd7876a62ecb31330346681e8017b3dba
Jason Riddle <hello@jasonriddle.com>
2023-04-27 12:02:38 -0700

    Comment out name and attributes

--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -22,23 +22,23 @@ variable "stage" {
   description = "ID element. Usually used to indicate role, e.g. 'prod', 'staging', 'source', 'build', 'test', 'deploy', 'release'"
 }
 
-variable "name" {
-  type        = string
-  default     = null
-  description = <<-EOT
-    ID element. Usually the component or solution name, e.g. 'app' or 'jenkins'.
-    This is the only ID element not also included as a `tag`.
-    The "name" tag is set to the full `id` string. There is no tag with the value of the `name` input.
-    EOT
-}
+# variable "name" {
+#   type        = string
+#   default     = null
+#   description = <<-EOT
+#     ID element. Usually the component or solution name, e.g. 'app' or 'jenkins'.
+#     This is the only ID element not also included as a `tag`.
+#     The "name" tag is set to the full `id` string. There is no tag with the value of the `name` input.
+#     EOT
+# }
 
-variable "attributes" {
-  type        = list(string)
-  default     = []
-  description = <<-EOT
-    ID element. Additional attributes (e.g. `workers` or `cluster`) to add to `id`,
-    in the order they appear in the list. New attributes are appended to the
-    end of the list. The elements of the list are joined by the `delimiter`
-    and treated as a single ID element.
-    EOT
-}
+# variable "attributes" {
+#   type        = list(string)
+#   default     = []
+#   description = <<-EOT
+#     ID element. Additional attributes (e.g. `workers` or `cluster`) to add to `id`,
+#     in the order they appear in the list. New attributes are appended to the
+#     end of the list. The elements of the list are joined by the `delimiter`
+#     and treated as a single ID element.
+#     EOT
+# }
