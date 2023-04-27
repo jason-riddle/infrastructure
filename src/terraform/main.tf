8ff4fd68b882a77510e8f1be283db1cd2451a8df
Jason Riddle <hello@jasonriddle.com>
2023-04-27 11:17:24 -0700

    Add console read only access

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,3 +1,8 @@
+module "terraform_aws_console_jason_read_only_access" {
+  source  = "./terraform_aws_console_jason_read_only_access"
+  enabled = var.enabled
+}
+
 module "terraform_github_jason_riddle_infrastructure" {
   source  = "./terraform_github_jason_riddle_infrastructure"
   enabled = var.enabled
