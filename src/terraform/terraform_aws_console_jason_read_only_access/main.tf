d49b79a608516942f9ecae14715d405a4bc7df00
Jason Riddle <hello@jasonriddle.com>
2023-04-27 11:16:29 -0700

    Rename to aws_console_jason_read_only_access

---
+++ src/terraform/terraform_aws_console_jason_read_only_access/main.tf
@@ -0,0 +1,22 @@
+locals {
+  enabled = var.enabled
+}
+
+# WIP: Enable login profile and fill in namespace, environment, etc.
+module "aws_console_jason_read_only_access" {
+  source  = "cloudposse/iam-user/aws"
+  version = "0.8.4"
+
+  # backups-servers-us-west-1-jasons-home-nas-backups
+  # namespace   = "my-namespace"
+  # environment = "my-env"
+  # stage       = ""
+  # name        = "my-name"
+  # attributes  = []
+
+  # Enable the IAM user
+  enabled               = local.enabled
+  login_profile_enabled = false
+  user_name             = "my_username"
+  pgp_key               = "my_pgp_key"
+}
