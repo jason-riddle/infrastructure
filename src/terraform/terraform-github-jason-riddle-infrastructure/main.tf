fc724c202b1446391137219b02e422cdee14136c
Jason Riddle <hello@jasonriddle.com>
2023-04-26 17:45:51 -0700

    Add github_branch_protection for infra

--- src/terraform/terraform-github-jason-riddle-infrastructure/main.tf
+++ src/terraform/terraform-github-jason-riddle-infrastructure/main.tf
@@ -1,3 +1,7 @@
 data "github_repository" "jason_riddle_infrastructure" {
   full_name = "jason-riddle/infrastructure"
 }
+
+resource "github_branch_protection" "jason_riddle_infrastructure" {
+  repository_id = github_repository.jason_riddle_infrastructure.node_id
+}
