b5591ad8a36aec90e0a43ea4adfb17d14ee8bfc9
Jason Riddle <contact+gh@jasonriddle.com>
2023-05-03 15:38:21 -0700

    Add AWS and GitHub comment

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,9 +1,13 @@
+## AWS
+
 module "terraform_aws_jason_console_read_only_access" {
   source  = "./terraform_aws_jason_console_read_only_access"
   enabled = var.enabled
 
   namespace = var.namespace
 }
+
+## GitHub
 
 module "terraform_github_jason_riddle_infrastructure" {
   source  = "./terraform_github_jason_riddle_infrastructure"
