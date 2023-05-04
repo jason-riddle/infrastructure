e8dc48c1dcf411a53b8a53117c82f7f440b2ed8e
Jason Riddle <contact@jasonriddle.com>
2023-05-04 16:35:59 -0700

    Comment out username

--- src/terraform/outputs.tf
+++ src/terraform/outputs.tf
@@ -1,5 +1,5 @@
-output "user_name" {
-  description = "jason user name"
-  value       = module.aws_console_iam_user.user_name
-  # value       = module.aws_console_iam_user.*.user_name
-}
+# output "user_name" {
+#   description = "jason user name"
+#   value       = module.aws_console_iam_user.user_name
+#   # value       = module.aws_console_iam_user.*.user_name
+# }
