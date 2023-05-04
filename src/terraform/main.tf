7d2af121f70dee1816e96ce9a91f55f4a189fa8e
Jason Riddle <contact@jasonriddle.com>
2023-05-04 10:54:34 -0700

    Add aws_console_iam_user_enabled var

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -18,7 +18,7 @@ module "aws_jason_console_read_only_access" {
   attributes  = var.attributes
 
   # Enable the IAM user
-  enabled               = local.enabled
+  enabled               = var.aws_console_iam_user_enabled
   login_profile_enabled = false
   user_name             = var.user_name
   pgp_key               = ""
--- src/terraform/variables-enabled.tf
+++ src/terraform/variables-enabled.tf
@@ -1,2 +1,5 @@
-variables
--enabled.tf
+variable "aws_console_iam_user_enabled" {
+  type        = bool
+  default     = true
+  description = "Set to false to prevent the module from creating any resources"
+}
