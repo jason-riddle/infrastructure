204f7f7a5d8b0c5929e1b663181403faf26b98fd
Jason Riddle <contact@jasonriddle.com>
2023-05-04 10:41:52 -0700

    Move module up

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -7,6 +7,23 @@
 #   namespace = var.namespace
 # }
 
+module "aws_jason_console_read_only_access" {
+  source  = "cloudposse/iam-user/aws"
+  version = "0.8.4"
+
+  namespace   = var.namespace
+  environment = var.environment
+  stage       = var.stage
+  name        = "jason-console-read-only-access"
+  attributes  = var.attributes
+
+  # Enable the IAM user
+  enabled               = local.enabled
+  login_profile_enabled = false
+  user_name             = var.user_name
+  pgp_key               = ""
+}
+
 ## Cloudflare
 
 ## DigitalOcean
