da8a01af36150f5c247bdb9cbf196b01d4907efe
Jason Riddle <contact@jasonriddle.com>
2023-05-04 17:02:23 -0700

    Add name

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -32,6 +32,7 @@ module "terraform_infrastructure_ci_user" {
   namespace   = var.namespace
   environment = var.environment
   stage       = var.stage
+  name        = "terraform-infrastructure-ci-user"
 
   ssm_enabled                   = false
   ssm_ses_smtp_password_enabled = false
