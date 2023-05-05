9bd1b76ba0d68cb5645e2186da52c6f875af72ad
Jason Riddle <contact@jasonriddle.com>
2023-05-04 17:00:24 -0700

    Don't enable

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -27,6 +27,10 @@
 module "terraform_infrastructure_ci_user" {
   source  = "cloudposse/iam-system-user/aws"
   version = "1.1.0"
+  enabled = false
+
+  ssm_enabled                   = false
+  ssm_ses_smtp_password_enabled = false
 }
 
 ## Cloudflare
