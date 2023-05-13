d285f886ded12231862dad9392d7edf845b0736b
Jason Riddle <contact@jasonriddle.com>
2023-05-12 18:21:46 -0700

    Comment out ci user name output

--- src/terraform/outputs.tf
+++ src/terraform/outputs.tf
@@ -4,6 +4,6 @@
 #   # value       = module.aws_console_iam_user.*.user_name
 # }
 
-output "terraform_infrastructure_aws_ci_user_name" {
-  value = module.terraform_infrastructure_aws_ci_user.user_name
-}
+# output "terraform_infrastructure_aws_ci_user_name" {
+#   value = module.terraform_infrastructure_aws_ci_user.user_name
+# }
