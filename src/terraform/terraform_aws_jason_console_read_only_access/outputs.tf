a619be84d7dd98ba1c6d93900d93c913636d6bc6
Jason Riddle <contact@jasonriddle.com>
2023-05-12 19:17:04 -0700

    Remove aws console read only user

--- src/terraform/terraform_aws_jason_console_read_only_access/outputs.tf
+++
@@ -1,5 +0,0 @@
-# output "user_name" {
-#   description = "jason user name"
-#   value       = module.aws_jason_console_read_only_access.user_name
-#   # value       = module.aws_jason_console_read_only_access.*.user_name
-# }
