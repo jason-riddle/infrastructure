dd137f8a10746b48275f76e5ae9122c3f3546abe
Jason Riddle <contact@jasonriddle.com>
2023-05-17 20:52:56 -0400

    Add other output sections

--- src/terraform/outputs.tf
+++ src/terraform/outputs.tf
@@ -1,3 +1,19 @@
+## AWS
+
 output "aws_iam_user_jason_user_name" {
   value = module.aws_iam_user_jason.user_name
 }
+
+## Cloudflare
+
+## DigitalOcean
+
+## GitHub
+
+## HCP
+
+## Tailscale
+
+## Terraform Cloud/Enterprise
+
+## Twilio
