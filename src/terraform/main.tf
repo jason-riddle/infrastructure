822a21ec8ab1f262d5fa8f07928997f108a7f434
Jason Riddle <contact@jasonriddle.com>
2023-06-01 18:15:23 -0700

    Remove jrapps.org resources

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -369,28 +369,6 @@ resource "cloudflare_record" "wildcard_jasonriddle_com_20_in1-smtp_messagingengi
 }
 
 # jrapps.org
-# data "cloudflare_zone" "jrapps_org" {
-#   name = "jrapps.org"
-# }
-
-# nextcloud.jrapps.org
-# resource "cloudflare_record" "jrapps_org_nx15310_your_storageshare_de" {
-#   count = 1
-
-#   zone_id = data.cloudflare_zone.jrapps_org.id
-#   name    = "nextcloud"
-#   value   = "nx15310.your-storageshare.de"
-#   comment = "Nextcloud"
-#   type    = "CNAME"
-#   ttl     = 1
-#   proxied = false
-
-#   lifecycle {
-#     prevent_destroy = true
-#   }
-# }
-
-# jrapps.org
 module "cloudflare_jrapps_org" {
   source  = "cloudposse/zone/cloudflare"
   version = "0.5.0"
