6f6710bf985c83b72d651c5535b5ba28d6ef0888
Jason Riddle <contact@jasonriddle.com>
2023-06-02 08:30:19 -0700

    Add subnet

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -81,23 +81,48 @@ module "iam_account_settings" {
   context = module.label.context
 }
 
-# module "vpc" {
-#   source  = "cloudposse/vpc/aws"
-#   version = "2.1.0"
-#   enabled = false
+locals {
+  # Prior to Kubernetes 1.19, the usage of the specific kubernetes.io/cluster/* resource tags below are required
+  # for EKS and Kubernetes to discover and manage networking resources
+  # https://www.terraform.io/docs/providers/aws/guides/eks-getting-started.html#base-vpc-networking
+  tags = { "kubernetes.io/cluster/${module.label.id}" = "shared" }
+}
 
-#   context = module.label.context
-# }
+module "vpc" {
+  source  = "cloudposse/vpc/aws"
+  version = "2.1.0"
+  enabled = false
 
-# module "eks_cluster" {
-#   source  = "cloudposse/eks-cluster/aws"
-#   version = "2.8.0"
-#   enabled = false
+  tags    = local.tags
+  context = module.label.context
+}
 
-#   vpc_id = module.vpc.vpc_id
+module "subnets" {
+  source  = "cloudposse/dynamic-subnets/aws"
+  version = "2.1.0"
+  enabled = false
 
-#   context = module.label.context
-# }
+  vpc_id = module.vpc.vpc_id
+
+  nat_gateway_enabled  = false
+  nat_instance_enabled = false
+
+  tags    = local.tags
+  context = module.label.context
+}
+
+
+module "eks_cluster" {
+  source  = "cloudposse/eks-cluster/aws"
+  version = "2.8.0"
+  enabled = false
+
+  vpc_id = module.vpc.vpc_id
+
+  subnet_ids = module.subnets.public_subnet_ids
+
+  context = module.label.context
+}
 
 module "ssm_patch_manager" {
   source  = "cloudposse/ssm-patch-manager/aws"
