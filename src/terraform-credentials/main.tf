28851e2335858e62eae9856653ff6bdbe1ef863d
Jason Riddle <contact@jasonriddle.com>
2023-06-11 11:24:15 -0700

    Unable to set var

--- src/terraform-credentials/main.tf
+++ src/terraform-credentials/main.tf
@@ -1,15 +1,26 @@
-data "tfe_organization" "jasonriddle" {
-  name = "org-jasonriddle"
-}
+# data "tfe_organization" "jasonriddle" {
+#   name = "org-jasonriddle"
+# }
+
+# resource "tfe_workspace" "terraform_infrastructure" {
+#   name         = "terraform-infrastructure"
+#   organization = data.tfe_organization.jasonriddle.name
+# }
+
+# data "tfe_workspace" "terraform_infrastructure" {
+#   name         = "terraform-infrastructure"
+#   organization = data.tfe_organization.jasonriddle.name
+# }
 
 data "tfe_workspace" "terraform_infrastructure" {
   name         = "terraform-infrastructure"
-  organization = data.tfe_organization.jasonriddle.name
+  organization = "org-jasonriddle"
 }
 
-resource "tfe_variable" "aws_region" {
-  key          = "AWS_REGION"
-  value        = "us-west-1"
-  category     = "env"
-  workspace_id = data.tfe_workspace.terraform_infrastructure.id
-}
+# TODO: Couldn't create env variable AWS_REGION_: resource not found
+# resource "tfe_variable" "aws_region" {
+#   key          = "AWS_REGION_"
+#   value        = "us-west-1"
+#   category     = "terraform"
+#   workspace_id = data.tfe_workspace.terraform_infrastructure.id
+# }
