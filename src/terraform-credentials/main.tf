772c43600d03e786b56c11ef824939536a2609d4
Jason Riddle <contact@jasonriddle.com>
2023-06-11 06:19:52 -0700

    Add workspace and variable

--- src/terraform-credentials/main.tf
+++ src/terraform-credentials/main.tf
@@ -1,3 +1,15 @@
 data "tfe_organization" "jasonriddle" {
   name = "org-jasonriddle"
 }
+
+data "tfe_workspace" "terraform_infrastructure" {
+  name         = "terraform-infrastructure"
+  organization = tfe_organization.jasonriddle.name
+}
+
+resource "tfe_variable" "foo_bar" {
+  key          = "FOO"
+  value        = "BAR"
+  category     = "env"
+  workspace_id = tfe_workspace.terraform_infrastructure.id
+}
